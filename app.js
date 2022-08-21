c4ce547ba54672b2bbcb66fe52e9df9ea969af10
farihaorna23 <farihaorna@gmail.com>
2022-08-21 14:32:27 -0400

    Finished exercise1

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const printOdds = count => {
+  if (count < 0) {
+    count = count * -1;
+  }
+
+  if (count === 0) {
+    console.log(`Please enter a number greater than 0`);
+  }
+  for (let i = 0; i <= count; i++) {
+    if (i % 2 !== 0) {
+      console.log(`the odd value is ${i}`);
+    }
+  }
+};
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
