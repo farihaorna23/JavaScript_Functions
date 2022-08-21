fda016072c6c61eac8a0d3e8a5ab0f7197590a42
farihaorna23 <farihaorna@gmail.com>
2022-08-21 15:25:50 -0400

    Finished exercise 3

--- app.js
+++ app.js
@@ -35,7 +35,40 @@ const checkAge = (userName = "Jane Doe", age = 0) => {
   }
 };
 
-checkAge("Arna", "24");
-checkAge("Sandy", 22);
-checkAge("Arnab", 15);
-checkAge();
+// checkAge("Arna", "24");
+// checkAge("Sandy", 22);
+// checkAge("Arnab", 15);
+// checkAge();
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+const quadrant = (x = 0, y = 0) => {
+  x = Number(x);
+  y = Number(y);
+  console.log(typeof x);
+  console.log(typeof y);
+  if (x === 0 && (y >= 0 || y <= 0)) {
+    console.log(`(${x}, ${y}) is on the y-axis`);
+  } else if ((x >= 0 || x <= 0) && y === 0) {
+    console.log(`(${x}, ${y}) is on the x-axis`);
+  } else if (x >= 1 && y >= 1) {
+    console.log(`(${x}, ${y}) is in Quadrant 1`);
+  } else if (x <= -1 && y >= 1) {
+    console.log(`(${x}, ${y}) is in Quadrant 2`);
+  } else if (x <= -1 && y <= -1) {
+    console.log(`(${x}, ${y}) is in Quadrant 3`);
+  } else if (x >= 1 && y <= -1) {
+    console.log(`(${x}, ${y}) is in Quadrant 4`);
+  }
+};
+
+// quadrant("2", "4");
+// quadrant(0, 2);
+// quadrant(1, 2);
+// quadrant(-6, 18);
+// quadrant(-3, 0); //x
+// quadrant(4, 0); //x
+// quadrant(0, 4); //y
+// quadrant(0, -5); //y
+// quadrant(-4, -4);
+// quadrant(2, -3);
