4772f20b0e100d5e98418998d8fcc2e17a44f2cf
farihaorna23 <farihaorna@gmail.com>
2022-08-21 14:48:56 -0400

    Finished exercise2

--- app.js
+++ app.js
@@ -17,5 +17,25 @@ const printOdds = count => {
   }
 };
 
+//printOdds(5);
+// printOdds(-5);
+// printOdds(0);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+const checkAge = (userName = "Jane Doe", age = 0) => {
+  age = Number(age);
+  //console.log(typeof age);
+  let aboveSixteen = `Congrats ${userName}, you can drive`;
+  let belowSixteen = `Sorry ${userName}, but you need to wait until you are 16.`;
+  if (age < 16) {
+    console.log(belowSixteen);
+  } else {
+    console.log(aboveSixteen);
+  }
+};
+
+checkAge("Arna", "24");
+checkAge("Sandy", 22);
+checkAge("Arnab", 15);
+checkAge();
